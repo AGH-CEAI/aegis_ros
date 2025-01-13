d7225ab8cccd5025ae954a01b88bab32f297c5d8
macmacal <mac.aleksandrowicz@gmail.com>
2025-01-13 18:30:12 +0100

    added ros2_net_ft_driver launch

---
+++ aegis_control/launch/ft_sensor_driver.launch.py
@@ -0,0 +1,18 @@
+from launch import LaunchDescription
+
+# Bypassing the launch system to access local import
+import os
+import sys
+
+run_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(run_path)
+from include.utils import controllers_spawner
+
+
+def generate_launch_description():
+    controllers_active = [
+        "net_ft_sensor_broadcaster",
+        "net_ft_diagnostic_broadcaster",
+    ]
+
+    return LaunchDescription([controllers_spawner(controllers_active)])
