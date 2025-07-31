a95b6f08619670b67941d1f695e6bc0bbf3091ca
Jakub <sivral@proton.me>
2025-07-31 16:25:03 +0200

    Add setup configuration

---
+++ aegis_utils/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = "aegis_utils"
+
+setup(
+    name=package_name,
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
+    data_files=[
+        ("share/ament_index/resource_index/packages", [f"resource/{package_name}"]),
+        (f"share/{package_name}", ["package.xml"]),
+        (f'share/{package_name}/config', [
+            'config/cam_scene.yaml',
+            'config/cam_tool_front.yaml',
+            'config/cam_tool_right.yaml',
+            'config/cam_tool_left.yaml'
+        ])
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="Jakub Plachno",
+    maintainer_email="sivral@proton.me",
+    description="A package",
+    license="Apache-2.0",
+    entry_points={
+        "console_scripts": [
+            "calib_data_collect = aegis_utils.calib_data_collect:main",
+            ],
+    },
+)
