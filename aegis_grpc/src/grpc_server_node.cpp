8f4cccd6e9c6c12948d44ebbeda5e82a04c4d23e
Maciej Aleksandrowicz <mac.aleksandrowicz@gmail.com>
2026-01-05 14:39:51 +0100

    CHG: added more logs

--- aegis_grpc/src/grpc_server_node.cpp
+++ aegis_grpc/src/grpc_server_node.cpp
@@ -32,16 +32,22 @@ int main(int argc, char *argv[]) {
 
   //TODO parametrize port
   const std::string address = "0.0.0.0:50051";
-  aegis_grpc::RobotReadServiceImpl read_service(node);
-  aegis_grpc::RobotControlServiceImpl control_service(node);
-  std::vector<grpc::Service *> services = {&read_service, &control_service};
 
+  std::cout << "Creating RobotReadService" << std::endl;
+  aegis_grpc::RobotReadServiceImpl read_service(node);
+
+  std::cout << "Creating RobotControlService" << std::endl;
+  aegis_grpc::RobotControlServiceImpl control_service(node);
+
+  std::cout << "Setuping gRPC server with services" << std::endl;
+  std::vector<grpc::Service *> services = {&read_service, &control_service};
   auto server = build_server(address, services);
   std::thread grpc_thread([&server]() { server->Wait(); });
   std::cout << "gRPC server listening on " << address << std::endl;
 
   rclcpp::spin(node);
 
+  std::cout << "Shutting down the gRPC server" << std::endl;
   server->Shutdown();
   grpc_thread.join();
   rclcpp::shutdown();
