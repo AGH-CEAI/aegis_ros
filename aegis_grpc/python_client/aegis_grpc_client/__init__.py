80c96183ad1730ccb73380161fc5622c5c0bfcf0
macmacal <mac.aleksandrowicz@gmail.com>
2026-07-22 14:56:55 +0200

    `aegis_grpc`: Server is now sorting joints order

--- aegis_grpc/python_client/aegis_grpc_client/__init__.py
+++ aegis_grpc/python_client/aegis_grpc_client/__init__.py
@@ -1,3 +1,19 @@
-from .grpc_client import AegisJointIndex, AegisRobotClient
+from .grpc_client import (
+    AegisJointIndex,
+    AegisRobotClient,
+    AegisJointName,
+    AEGIS_JOINTS_ORDER,
+    CameraName,
+    ModalityGroup,
+    StateModality,
+)
 
-__all__ = ["AegisJointIndex", "AegisRobotClient"]
+__all__ = [
+    "AEGIS_JOINTS_ORDER",
+    "AegisJointIndex",
+    "AegisJointName",
+    "AegisRobotClient",
+    "CameraName",
+    "ModalityGroup",
+    "StateModality",
+]
